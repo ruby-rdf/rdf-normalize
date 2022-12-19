72d8f155867a0ef100623f352e4882b2c94cd6b1
Gregg Kellogg <gregg@greggkellogg.net>
2022-12-19 13:37:47 -0800

    Update logging based on proposal.

--- lib/rdf/normalize/urdna2015.rb
+++ lib/rdf/normalize/urdna2015.rb
@@ -1,3 +1,5 @@
+require 'rdf/nquads'
+
 module RDF::Normalize
   class URDNA2015
     include RDF::Enumerable
@@ -20,37 +22,37 @@ module RDF::Normalize
 
     protected
     def normalize_statements(ns, &block)
-      # Map BNodes to the statements they are used by
+      # Step 2: Map BNodes to the statements they are used by
       dataset.each_statement do |statement|
         statement.to_quad.compact.select(&:node?).each do |node|
           ns.add_statement(node, statement)
         end
       end
+      log_debug("ca", "step 2") {"bn to quads: #{ns.inspect_bnode_to_statements}"}
 
       ns.hash_to_bnodes = {}
 
-      # Calculate hashes for first degree nodes
+      # Step 3: Calculate hashes for first degree nodes
       ns.bnode_to_statements.each_key do |node|
-        log_debug("ca") {"1deg: #{node}"}
         hash = log_depth {ns.hash_first_degree_quads(node)}
         ns.add_bnode_hash(node, hash)
       end
 
-      # Create canonical replacements for hashes mapping to a single node
+      # Step 4: Create canonical replacements for hashes mapping to a single node
       ns.hash_to_bnodes.keys.sort.each do |hash|
         identifier_list = ns.hash_to_bnodes[hash]
         next if identifier_list.length > 1
         node = identifier_list.first
         id = ns.canonical_issuer.issue_identifier(node)
-        log_debug("ca", "single node") {"node: #{node.to_ntriples}, hash: #{hash}, id: #{id}"}
+        log_debug("ca", "step 4.2") {"identifier: #{node.id}, hash: #{hash}, cid: #{id[2..-1]}"}
         ns.hash_to_bnodes.delete(hash)
       end
 
-      # Iterate over hashs having more than one node
+      # Step 5: Iterate over hashs having more than one node
       ns.hash_to_bnodes.keys.sort.each do |hash|
         identifier_list = ns.hash_to_bnodes[hash]
 
-        log_debug("ca", "multiple nodes") {"node: #{identifier_list.map(&:to_nquads).map(&:strip).join(",")}, hash: #{hash}"}
+        log_debug("ca", "step 5.1") {"identifier_list: #{identifier_list.map(&:to_nquads).map(&:strip).join(",")}, hash: #{hash}"}
         hash_path_list = []
 
         # Create a hash_path_list for all bnodes using a temporary identifier used to create canonical replacements
@@ -60,18 +62,18 @@ module RDF::Normalize
           temporary_issuer.issue_identifier(identifier)
           hash_path_list << log_depth {ns.hash_n_degree_quads(identifier, temporary_issuer)}
         end
-        log_debug("ca") {"hash_path_list: #{hash_path_list.inspect}"}
+        log_debug("ca", "step 5.2") {"hash_path_list: #{hash_path_list.inspect}"}
 
         # Create canonical replacements for nodes
         hash_path_list.sort_by(&:first).map(&:last).each do |issuer|
           issuer.issued.each do |node|
             id = ns.canonical_issuer.issue_identifier(node)
-            log_debug("-->") {"node: #{node.to_ntriples}, id: #{id}"}
+            log_debug("ca", "step 5.3.1") {"node: #{node.id}, cid: #{id}"}
           end
         end
       end
 
-      # Yield statements using BNodes from canonical replacements
+      # Step 6: Yield statements using BNodes from canonical replacements
       dataset.each_statement do |statement|
         if statement.has_blank_nodes?
           quad = statement.to_quad.compact.map do |term|
@@ -82,6 +84,9 @@ module RDF::Normalize
           block.call statement
         end
       end
+
+      log_debug("ca", "exit") {ns.canonical_issuer.inspect}
+      dataset
     end
 
   private
@@ -114,7 +119,7 @@ module RDF::Normalize
       # @param [RDF::Node] node The reference blank node identifier
       # @return [String] the SHA256 hexdigest hash of statements using this node, with replacements
       def hash_first_degree_quads(node)
-        log_debug("1deg") {"input: #{bnode_to_statements[node].map(&:to_nquads).map(&:strip).join(' ')}"}
+        log_debug("h1d", "entry") {"identifier: #{node.id}"}
         nquads = bnode_to_statements[node].
           map do |statement|
             quad = statement.to_quad.map do |t|
@@ -124,12 +129,12 @@ module RDF::Normalize
               else t
               end
             end
-            RDF::NQuads::Writer.serialize(RDF::Statement.from(quad))
+            RDF::Statement.from(quad).to_nquads
           end
+        log_debug("h1d", "exit") {"nquads: #{nquads.map(&:strip)}"}
 
-        log_debug("1deg") {"nquads: #{nquads.map(&:strip).join(' ')}"}
         result = hexdigest(nquads.sort.join)
-        log_debug("1deg") {"hash: #{result}"}
+        log_debug("h1d", "exit") {"hash: #{result}"}
         result
       end
 
@@ -139,15 +144,17 @@ module RDF::Normalize
       # @param [String] position one of :s, :o, or :g
       # @return [String] the SHA256 hexdigest hash
       def hash_related_node(related, statement, issuer, position)
-        log_debug("hrel") {"related: #{related.to_nquads}, position: #{position}"}
-        log_debug("hrel") {"predicate: #{statement.predicate.to_nquads}, position: #{position}"}
-        identifier = canonical_issuer.identifier(related) ||
-                     issuer.identifier(related) ||
-                     hash_first_degree_quads(related)
+        log_debug("hrbn", "entry") {"related: #{related.to_nquads}, position: #{position}"}
+        log_debug("hrbn", "entry") {"quad: #{statement.to_nquads.strip}"}
         input = "#{position}"
         input << statement.predicate.to_ntriples unless position == :g
-        input << identifier
-        log_debug("hrel") {"input: #{input.inspect}, hash: #{hexdigest(input)}"}
+        if identifier = (canonical_issuer.identifier(related) ||
+                         issuer.identifier(related))
+          input << identifier.to_s
+        else
+          input << log_depth {hash_first_degree_quads(related)}
+        end
+        log_debug("hrbn", "exit") {"input: #{input.inspect}, hash: #{hexdigest(input)}"}
         hexdigest(input)
       end
 
@@ -155,34 +162,37 @@ module RDF::Normalize
       # @param [IdentifierIssuer] issuer
       # @return [Array<String,IdentifierIssuer>] the Hash and issuer
       def hash_n_degree_quads(identifier, issuer)
-        log_debug("ndeg") {"canon issuer: #{canonical_issuer.inspect}"}
-        log_debug("ndeg") {"identifier: #{identifier.to_ntriples}"}
-        log_debug("ndeg") {"issuer: #{issuer.inspect}"}
+        log_debug("hndq", "entry") {"identifier: #{identifier.id}"}
+        log_debug("hndq", "entry") {issuer.inspect}
 
         # hash to related blank nodes map
         hn = {}
 
-        log_debug("ndeg") {"quads: #{bnode_to_statements[identifier].map(&:to_nquads).map(&:strip).join(' ')}"}
+        log_debug("hndq", "step 2") {"quads: #{bnode_to_statements[identifier].map(&:to_nquads).map(&:strip).join(' ')}"}
 
+        # Step 3
         bnode_to_statements[identifier].each do |statement|
           log_depth {hash_related_statement(identifier, statement, issuer, hn)}
         end
-        log_debug("ndeg") {"hn: #{hn.map {|h,l| "#{h}: #{l.map(&:to_ntriples)}"}.join('; ')}"}
+        log_debug("hndq", "step 3") {"hn: #{hn.map {|h,l| "#{h}: #{l.map(&:to_ntriples)}"}.join('; ')}"}
 
         data_to_hash = ""
 
         log_depth do
+          # Step 5
           hn.keys.sort.each do |hash|
+            log_debug("hndq", "step 5") {"hash: #{hash}, data_to_hash: #{data_to_hash}"}
             list = hn[hash]
             # Iterate over related nodes
             chosen_path, chosen_issuer = "", nil
             data_to_hash += hash
 
             list.permutation do |permutation|
-              log_debug("ndeg") {"perm: #{permutation.map(&:to_ntriples).join(", ")}"}
+              log_debug("hndq", "step 5.4") {"perm: #{permutation.map(&:id).join(", ")}"}
               issuer_copy, path, recursion_list = issuer.dup, "", []
 
               permutation.each do |related|
+                log_debug("hndq", "step 5.4.4") {"related: #{related}, path: #{path}"}
                 if canonical_issuer.identifier(related)
                   path << canonical_issuer.issue_identifier(related)
                 else
@@ -193,13 +203,14 @@ module RDF::Normalize
                 # Skip to the next permutation if chosen path isn't empty and the path is greater than the chosen path
                 break if !chosen_path.empty? && path.length >= chosen_path.length
               end
-              log_debug("ndeg") {"hash: #{hash}, path: #{path}, recursion: #{recursion_list.map(&:to_ntriples)}"}
 
+              log_debug("hndq", "step 5.4.5") {"recursion_list: #{recursion_list.map(&:to_ntriples)}, path: #{path}"}
               recursion_list.each do |related|
                 result = log_depth {hash_n_degree_quads(related, issuer_copy)}
                 path << issuer_copy.issue_identifier(related)
                 path << "<#{result.first}>"
                 issuer_copy = result.last
+                log_debug("hndq", "step 5.4.5.5") {"path: #{path}, issuer copy: #{issuer_copy.inspect}"}
                 break if !chosen_path.empty? && path.length >= chosen_path.length && path > chosen_path
               end
 
@@ -209,12 +220,26 @@ module RDF::Normalize
             end
 
             data_to_hash += chosen_path
+            log_debug("hndq", "step 5.6") {"chosen_path: #{chosen_path}, data_to_hash: #{data_to_hash}"}
             issuer = chosen_issuer
           end
         end
 
-        log_debug("ndeg") {"datatohash: #{data_to_hash.inspect}, hash: #{hexdigest(data_to_hash)}"}
+        log_debug("hndq") {"hash: #{hexdigest(data_to_hash)}, #{issuer.inspect}"}
         return [hexdigest(data_to_hash), issuer]
+      end
+
+      def inspect
+        "NormalizationState:\nbnode_to_statements: #{inspect_bnode_to_statements}\nhash_to_bnodes: #{inspect_hash_to_bnodes}\ncanonical_issuer: #{canonical_issuer.inspect}"
+      end
+
+      def inspect_bnode_to_statements
+        bnode_to_statements.map do |n, statements|
+          "#{n.id}: #{statements.map {|s| s.to_nquads.strip}}"
+        end.join(", ")
+      end
+
+      def inspect_hash_to_bnodes
       end
 
       protected
