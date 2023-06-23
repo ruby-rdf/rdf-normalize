8e7141a2db0523987fc4e3541bf91a4340e60a85
Gregg Kellogg <gregg@greggkellogg.net>
2023-06-23 15:27:44 -0700

    Add back writer tests, but remove all but the first thirty.

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -14,4 +14,21 @@ describe RDF::Normalize::Writer do
       expect(RDF::Writer.for(:normalize)).to eql described_class
     end
   end
+
+  [:urgna2012, :urdna2015].each do |algorithm|
+    describe algorithm do
+      describe "w3c normalization tests" do
+        Dir.glob(File.expand_path("../data/*-in.nq", __FILE__)).each do |input|
+          next unless File.exist?(input.sub("-in", "-#{algorithm}"))
+          it "produces expected output for #{input.split('/').last}" do
+            expected = File.read(input.sub("-in", "-#{algorithm}"))
+            input_data = File.read(input)
+            repo = RDF::Repository.load(input)
+            result = repo.dump(:normalize, algorithm: (algorithm == :urdna2015 ? :rdfc10 : algorithm), logger: logger)
+            expect(result).to produce(expected, id: input, result: repo.dump(:nquads, validate: false), logger: logger)
+          end
+        end
+      end
+    end
+  end
 end
